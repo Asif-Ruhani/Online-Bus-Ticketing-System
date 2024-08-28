59ae9dfd0ec9d7ae067359f980d6eeaa1376062f
Asif-Ruhani <asifruhani68@gmail.com>
2024-08-28 20:45:25 +0600

    changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let i=0;
 let totalPrice=0;
 let GrandTotal=0;
 let TotalSeat=0;
+const SeatNames=[A1,A2,A3,A4,B1,B2,B3,B4,C1,C2,C3,C4,D1,D2,D3,D4,E1,E2,E3,E4,F1,F2,F3,F4,G1,G2,G3,G4,H1,H2,H3,H4,I1,I2,I3,I4,J1,J2,J3,J4,J5];
 
 let CurrentAvailableSeats=document.getElementById('AvailableSeats');
 CurrentAvailableSeats.innerText=availableSeats;
@@ -26,6 +27,24 @@ Grand_Total.innerText=GrandTotal;
 GetSeatName(); // whend the project is run, this function will be called
 
 
+function GetSeatName(){
+          
+    document.addEventListener('click',function(event){   // get the button name that is clicked bu mouse
+        const SeatName=event.target.innerText;
+        // console.log('now seat name is is ',SeatName);
+      
+        for(let i=0;i<40;i++){
+            if(SeatName == SeatNames[i].innerText){
+                SeatAllocation(SeatName); 
+                break;
+            }
+        }  
+    
+      })
+
+  }
+
+
 
 
  function SeatAllocation(seatname){
@@ -39,6 +58,7 @@ GetSeatName(); // whend the project is run, this function will be called
              // One person can not reserve more than 4 seats 
              if(ReservedSeats.length<4){
                  ReservedSeats.push(seatname);
+                 console.log('Reseved seats are: ',ReservedSeats);
                 i++;
 
                 //  Update available & reserved seats
@@ -84,6 +104,8 @@ GetSeatName(); // whend the project is run, this function will be called
                 // Set the total price & grand price.
                  totalprice.innerText=totalPrice;
                  Grand_Total.innerText=totalPrice;
+
+                 TotalSeat=TotalSeat+1;
             }
     
         } 
@@ -93,8 +115,15 @@ GetSeatName(); // whend the project is run, this function will be called
 
 document.getElementById('ApplyCoupon').addEventListener('click',function(){  // this line execute when "Apply Coupon" button is clicked.
     
-    const DiscountDisplay=document.getElementById('DiscountInfoDisplay');
-        const DiscountInputBox=document.getElementById('inputBox');
+       if(TotalSeat<2){
+        const DiscountAlarm=document.getElementById('DiscountAlarm');
+        DiscountAlarm.classList.remove('DisplayHidden');
+       }
+    
+        else{
+                 
+            const DiscountDisplay=document.getElementById('DiscountInfoDisplay');
+            const DiscountInputBox=document.getElementById('inputBox');
 
                       if(DiscountInputBox.value === 'NEW15'){
                         Grand_Total.innerText=totalPrice-(totalPrice*0.15);
@@ -113,20 +142,34 @@ document.getElementById('ApplyCoupon').addEventListener('click',function(){  // 
                           
                     }
 
+        }
+
 })
 
+ 
+ document.getElementById('DiscountAlarmButton').addEventListener('click',function(){
+    const DiscountAlarm=document.getElementById('DiscountAlarm');
+    DiscountAlarm.classList.add('DisplayHidden');
+ })
 
 
-
-    function GetSeatName(){
-          
-    document.addEventListener('click',function(event){   // get the button name that is clicked bu mouse
-        const SeatName=event.target.innerText;
-        SeatAllocation(SeatName);
+  document.getElementById('TicketConfirmButton').addEventListener('click',function(){
     
-      })
+    
+    
+    
+    const name=document.getElementById('Name');
+    name.value='';
 
-  }
+    const phone=document.getElementById('Phone');
+    phone.value='';
+
+    const NID=document.getElementById('NID');
+    NID.value='';
+
+  })
+
+
 
 
 
